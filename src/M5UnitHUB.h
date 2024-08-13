1efd7016ad34fce0e54df2b78c3ec12ce572fcf5
GOB <goblin52@gmail.com>
2024-08-13 13:18:00 +0900

    Add headers and some tweaks

---
+++ src/M5UnitHUB.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*!
+  @file M5UnitHUB.h
+ */
+#ifndef M5_UNIT_HUB_H
+#define M5_UNIT_HUB_H
+#ifdef __cplusplus
+#include "M5UnitHUB.hpp"
+#else
+#error M5UnitHUB requires a C++ compiler, please change file extension to .cc or .cpp
+#endif
+#endif
---
+++ src/M5UnitHUB.hpp
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*!
+  @file M5UnitHUB.hpp
+  @brief Main header of M5UnitHUB
+
+  @mainpage M5UnitHUB
+  Library for UnitHUB using M5UnitUnified.
+*/
+#ifndef M5_UNIT_HUB_HPP
+#define M5_UNIT_HUB_HPP
+
+#include "unit/unit_PaHub.hpp"
+
+/*!
+  @namespace m5
+  @brief Top level namespace of M5stack
+ */
+namespace m5 {
+
+/*!
+  @namespace unit
+  @brief Unit-related namespace
+ */
+namespace unit {}  // namespace unit
+}  // namespace m5
+
+#endif
